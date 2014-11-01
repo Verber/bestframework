c9aac77a2d38b1cabd4ce7487ff98f5bb9f4c3f3
Ivan Mosiev <i.k.mosev@gmail.com>
2014-11-01 12:06:28 +0200

    Get me env pls

--- rest.php
+++ rest.php
@@ -7,4 +7,8 @@ $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name) . '. Welcome to Azure!';
 });
 
+$app->get('/framework', function ($name) use ($app) {
+    return json_encode($_ENV);
+});
+
 $app->run();
---
+++ src/Rest/Framework.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 01.11.14
+ * Time: 11:29
+ */
+
+namespace Rest;
+
+use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Common\ServiceException;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use WindowsAzure\Table\Models\EdmType;
+use WindowsAzure\Table\Models\Entity;
+
+
+class Framework {
+
+} 
