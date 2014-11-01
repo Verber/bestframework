dc205cbb9f45332ec4d9b6dd94dd8ffbd1f341c0
Ivan Mosiev <i.k.mosev@gmail.com>
2014-11-01 12:53:20 +0200

    Fixed copy-paste error

--- rest.php
+++ rest.php
@@ -2,12 +2,13 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name) . '. Welcome to Azure!';
 });
 
-$app->get('/framework', function ($name) use ($app) {
+$app->get('/framework', function () use ($app) {
     return json_encode($_ENV);
 });
 
