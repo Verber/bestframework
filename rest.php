9995d72855e6fb94484e0b7fe25c4d6e59e11e06
Ivan Mosiev <i.k.mosev@gmail.com>
2014-11-03 08:20:55 +0200

    Added api methods

--- rest.php
+++ rest.php
@@ -1,15 +1,28 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
+use WindowsAzure\Common\ServicesBuilder;
+
 $app = new Silex\Application();
 $app['debug'] = true;
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name) . '. Welcome to Azure!';
+$app['storage_connection_string'] = isset($_SERVER['CUSTOMCONNSTR_STORAGE'])?
+    $_SERVER['CUSTOMCONNSTR_STORAGE']
+    :getenv('CUSTOMCONNSTR_STORAGE');
+$app['azure.table'] = $app->share(function ($app) {
+    return ServicesBuilder::getInstance()->createTableService($app['storage_connection_string']);
 });
 
-$app->get('/framework', function () use ($app) {
-    return json_encode([$_SERVER, $_ENV]);
+$app->get('/storage/init', function() use ($app) {
+    $existingTables = $app['azure.table']->queryTables('frameworks')->getTables();
+    if (count($existingTables) == 0) {
+        $app['azure.table']->createTable('frameworks');
+        return json_encode(array('status' => 'Table created'));
+    }
+    return json_encode(array('status' => 'Table exists'));
 });
+
+$app->get('/framework', 'Rest\Framework::get');
+$app->post('/framework', 'Rest\Framework::post');
+$app->put('/framework', 'Rest\Framework::put');
 
 $app->run();
--- src/Rest/Framework.php
+++ src/Rest/Framework.php
@@ -8,14 +8,95 @@
 
 namespace Rest;
 
-use WindowsAzure\Common\ServicesBuilder;
-use WindowsAzure\Common\ServiceException;
+use Client\WindowsAzureErrorCodes;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use WindowsAzure\Table\Internal\ITable;
 use WindowsAzure\Table\Models\EdmType;
 use WindowsAzure\Table\Models\Entity;
+use WindowsAzure\Table\Models\GetEntityResult;
 
 
-class Framework {
+class Framework
+{
+
+    private $tableProxy;
+
+    /**
+     * Returns json-encoded list of frameworks with number of likes
+     * @param Request $request
+     * @param Application $app
+     * @return string
+     */
+    public function get(Request $request, Application $app)
+    {
+        $frameworks = $app['azure.table']
+                ->queryEntities("frameworks")
+                ->getEntities();
+        $response = [];
+        /**
+         * @var Entity $framework
+         */
+        foreach ($frameworks as $framework) {
+            $response[] = [
+                'key' => $framework->getRowKey(),
+                'name' => $framework->getPropertyValue('name'),
+                'votes' => $framework->getPropertyValue('votes')
+            ];
+        }
+        return json_encode($response);
+    }
+
+    /**
+     * Create new framework
+     * @param Request $request
+     * @param Application $app
+     */
+    public function post(Request $request, Application $app)
+    {
+        $name = $request->get('name');
+        $key = md5($request->get('name'));
+
+        $framework = new Entity();
+        $framework->setPartitionKey('framework');
+        $framework->setRowKey($key);
+        $framework->addProperty('name', EdmType::STRING, $name);
+        try {
+            $app['azure.table']->insertEntity('frameworks', $framework);
+            return json_encode(['key' => $key]);
+        } catch (\Exception $e) {
+            return json_encode(['type'=> get_class($e), 'message' => $e->getMessage()]);
+        }
+    }
+
+    /**
+     * Vote for framework
+     * @param Request $request
+     * @param Application $app
+     */
+    public function put(Request $request, Application $app)
+    {
+        /**
+         * @var ITable $table
+         */
+        $table = $app['azure.table'];
+        try {
+            /**
+             * @var Entity $framework
+             */
+            $framework = $table
+                ->getEntity('frameworks', 'framework', $request->get('key'))
+                ->getEntity();
+            if (null === $framework->getPropertyValue('votes')) {
+                $framework->addProperty('votes', EdmType::INT32, 1);
+            } else {
+                $framework->setPropertyValue('votes', $framework->getPropertyValue('votes') + 1);
+            }
+            $table->mergeEntity('frameworks', $framework);
+            return json_encode(['key' => $framework->getRowKey(), 'votes' => $framework->getPropertyValue('votes')]);
+        } catch (\Exception $e) {
+            return json_encode(['type' => get_class($e), 'message' => $e->getMessage()]);
+        }
+    }
 
 } 
